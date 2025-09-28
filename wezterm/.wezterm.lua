0c31cb0e4f7ed17f1fe6ade3ed3929bad926b2d9
lcsh0s <lucasdc@amo.co>
2025-09-28 02:04:26 +0200

    new condfig

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,15 +10,14 @@ config.audible_bell = "Disabled"
 
 -- Window
 config.window_decorations = "RESIZE"
-config.enable_tab_bar = false
-
-config.window_padding = {
-	left = "1cell",
-	right = "1cell",
-	top = "0.5cell",
-	bottom = 0,
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+config.tab_max_width = 32
+config.window_frame = {
+	border_top_height = "3px",
 }
-
 -- Keys --
 config.keys = {
 	{
@@ -37,5 +36,45 @@ config.keys = {
 		action = wezterm.action({ SendString = "\x1bOF" }),
 	},
 }
+
+-- Plugins --
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+tabline.setup({
+	options = {
+		icons_enabled = true,
+		theme = "Catppuccin Mocha",
+		tabs_enabled = true,
+		theme_overrides = {},
+		section_separators = {
+			left = wezterm.nerdfonts.pl_left_hard_divider,
+			right = wezterm.nerdfonts.pl_right_hard_divider,
+		},
+		component_separators = {
+			left = wezterm.nerdfonts.pl_left_soft_divider,
+			right = wezterm.nerdfonts.pl_right_soft_divider,
+		},
+		tab_separators = {
+			left = wezterm.nerdfonts.pl_left_hard_divider,
+			right = wezterm.nerdfonts.pl_right_hard_divider,
+		},
+	},
+	sections = {
+		tabline_a = { "mode" },
+		tabline_b = { { "", cond = false } },
+		tabline_c = { { "", cond = false } },
+		tab_active = {
+			"index",
+			{ "parent", padding = 0 },
+			"/",
+			{ "cwd", padding = { left = 0, right = 1 } },
+		},
+		tab_inactive = { "index", { "process", padding = { left = 0, right = 1 }, icons_enabled = false } },
+		tabline_x = { "ram", "cpu" },
+		tabline_y = { { "", cond = false } },
+		tabline_z = { { "", cond = false } },
+	},
+	extensions = {},
+})
+tabline.apply_to_config(config)
 
 return config
