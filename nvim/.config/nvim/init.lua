c61012d592859228816e057fbd28b365b12d5932
lcsh0s <lucasdc@amo.co>
2025-10-27 22:12:21 +0100

    .

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -317,6 +317,47 @@ require('lazy').setup({
       }
     end,
   },
+
+  {
+    --"mrcjkb/rustaceanvim",
+    dir = '~/dotfiles/nvim/.config/nvim/lazy-plugins/rustaceanvim/',
+    --version = false, -- Recommended
+    ft = { 'rust' },
+    opts = {
+      server = {
+        load_vscode_settings = false,
+        root_dir = function(fname)
+          return require('lspconfig.util').root_pattern 'Cargo.toml'(fname)
+            or require('lspconfig.util').root_pattern 'rust-analyzer.json'(fname)
+            or require('lspconfig.util').find_git_ancestor(fname)
+        end,
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ['rust-analyzer'] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              buildScripts = {
+                enable = true,
+              },
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = true,
+            procMacro = {
+              enable = true,
+            },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      vim.g.rustaceanvim = vim.tbl_deep_extend('keep', vim.g.rustaceanvim or {}, opts or {})
+      if vim.fn.executable 'rust-analyzer' == 0 then
+        LazyVim.error('**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/', { title = 'rustaceanvim' })
+      end
+    end,
+  },
+
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
