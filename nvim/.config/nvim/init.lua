951c12e6d43645932f0cc8bf28c160fd3ff16f17
lcsh0s <lucasdc@amo.co>
2025-10-27 16:01:36 +0100

    added supermaven

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -293,7 +293,29 @@ require('lazy').setup({
   --
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
-
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {
+        keymaps = {
+          accept_suggestion = '<C-m>',
+          clear_suggestion = '<C-]>',
+          accept_word = '<C-j>',
+        },
+        ignore_filetypes = {}, -- or { "cpp", }
+        -- color = {
+        --   suggestion_color = '#ffffff',
+        --   cterm = 244,
+        -- },
+        log_level = 'off', -- set to "off" to disable logging completely
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+        condition = function()
+          return false
+        end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+      }
+    end,
+  },
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
