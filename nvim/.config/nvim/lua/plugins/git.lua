524c195544c3b5c39d86f23d187b427ab9911951
lcsh0s <lucasdc@amo.co>
2025-10-31 17:29:42 +0100

    removed backup (as it is in version control) and removed lazygit

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -13,22 +13,4 @@ return {
       },
     },
   },
-
-  { -- LazyGit integration
-    'kdheepak/lazygit.nvim',
-    lazy = true,
-    cmd = {
-      'LazyGit',
-      'LazyGitConfig',
-      'LazyGitCurrentFile',
-      'LazyGitFilter',
-      'LazyGitFilterCurrentFile',
-    },
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    keys = {
-      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-    },
-  },
 }
