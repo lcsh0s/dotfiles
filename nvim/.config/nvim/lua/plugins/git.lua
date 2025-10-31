86817dafe18874ccbe524dbfe758eb8e6c7d42c5
lcsh0s <lucasdc@amo.co>
2025-10-31 17:34:43 +0100

    .

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -13,4 +13,12 @@ return {
       },
     },
   },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+  },
 }
