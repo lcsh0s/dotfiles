729e8f66354a6dff5411b30d91318ae76f2acb4d
lcsh0s <lucasdc@amo.co>
2025-11-05 16:18:30 +0100

    noice: removed custom on cmd

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -41,36 +41,6 @@ return {
           view_history = 'messages', -- view for :messages
           view_search = 'virtualtext',
         },
-        views = {
-          cmdline_popup = {
-            position = {
-              row = 5,
-              col = '50%',
-            },
-            size = {
-              width = 60,
-              height = 'auto',
-            },
-          },
-          popupmenu = {
-            relative = 'editor',
-            position = {
-              row = 20,
-              col = '50%',
-            },
-            size = {
-              width = 60,
-              height = 10,
-            },
-            border = {
-              style = 'rounded',
-              padding = { 0, 1 },
-            },
-            win_options = {
-              winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
-            },
-          },
-        },
         lsp = {
           hover = {
             enabled = true,
