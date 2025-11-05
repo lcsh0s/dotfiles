0b46f2828d34291f1059522ac176d15eb62c2886
lcsh0s <lucasdc@amo.co>
2025-11-05 15:39:40 +0100

    added gh dash plugin

--- nvim/.config/nvim/lua/plugins/giet.lua
+++ nvim/.config/nvim/lua/plugins/giet.lua
@@ -32,4 +32,27 @@ return {
       end, { desc = 'Open Neogit on split' })
     end,
   },
+  {
+    'johnseth97/gh-dash.nvim',
+    lazy = true,
+    keys = {
+      {
+        '<leader>gd',
+        function()
+          require('gh_dash').toggle()
+          vim.schedule(function()
+            vim.cmd 'startinsert'
+          end)
+        end,
+        desc = 'Toggle gh-dash popup',
+      },
+    },
+    opts = {
+      keymaps = {},
+      border = 'rounded',
+      width = 0.8,
+      height = 0.8,
+      autoinstall = true,
+    },
+  },
 }
